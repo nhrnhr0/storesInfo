2ee9961675f5f67331e5717d251399fba586c204
nhrnhr0 <nhrnhr0@gmail.com>
2022-05-23 16:51:23 +0300

    fix file export

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from django.contrib import admin
 from django.http import HttpResponse
 from .models import Nearbysearch, Place, EasyList, EasyPage, EasyCategory
@@ -32,9 +33,10 @@ class PlaceAdmin(AdminAdvancedFiltersMixin, admin.ModelAdmin):
         )
         response.write(u'\ufeff'.encode('utf8'))
         writer = csv.writer(response)
-        writer.writerow(['name', 'address', 'phone', 'types', 'detailResponse'])
+        writer.writerow(['name', 'address', 'phone','Place id', 'x', 'y', 'url'])
         for place in queryset:
-            writer.writerow([place.name, place.address, place.formated_phone, place.types, place.detailResponse])
+            if place.formated_phone:
+                writer.writerow([place.name, place.address, place.formated_phone, place.place_id, place.x, place.y, place.url])
         return response
         
     advanced_filter_fields = ('detailResponse','name','address','rating','place_id','types','vicinity','x','y','nearbysearch','businessStatus','permenentlyClose','userRatingTotal','formated_phone','internal_phone','url','createdAt','updatedAt','website','addressComponents')
@@ -44,11 +46,28 @@ class EasyPageAdmin(AdminAdvancedFiltersMixin, admin.ModelAdmin):
     search_fields = ['name', 'url']
     list_display = ('id', 'name', 'page_id', 'hasData', 'url',)
     advanced_filter_fields = ('name', 'url')
-    actions = ('load_page_data_to_empty_places',)
+    actions = ('load_page_data_to_empty_places','download_to_csv')
     def load_page_data_to_empty_places(self, request, queryset):
         for page in queryset:
             if page.hasData == False:
                 page.easy_data_request()
+    def download_to_csv(self, request, queryset):
+        response = HttpResponse(
+        content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
+        )
+        response.write(u'\ufeff'.encode('utf8'))
+        writer = csv.writer(response)
+        writer.writerow(['name', 'url', 'page_id', 'phone', 'lat', 'lng',])
+        for page in queryset:
+            if page.hasData and page.html:
+                bs = BeautifulSoup(page.html, 'html.parser')
+                #phone = bs.find('#action-side-2-label')
+                #addr = bs.find('.biz-address-text')
+                #if addr:
+                    #addr = addr.text
+                writer.writerow([page.name, page.url, page.page_id, page.phone, page.lat, page.lng])
+        return response
 admin.site.register(EasyPage, EasyPageAdmin)
 
 class EasyListAdmin(AdminAdvancedFiltersMixin, admin.ModelAdmin):
